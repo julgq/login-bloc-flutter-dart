46de349b9b40fc04d324c470458b9d56ddbe0311
Julio <jc.gutierrezq@gmail.com>
2019-05-01 13:51:11 -0500

    broadcast

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 import 'blocs/provider.dart';
 
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
