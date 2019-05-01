95e42c9191b1ca00dbc23f019eba94737909365a
Julio <jc.gutierrezq@gmail.com>
2019-05-01 14:51:22 -0500

    BehaviorSubject and config submit()

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -65,11 +65,7 @@ Widget submitButton(Bloc bloc) {
       return RaisedButton(
         child: Text('Login'),
         color: Colors.blue,
-        onPressed: snapshot.hasData
-            ? () {
-                print('Hi there!');
-              }
-            : null,
+        onPressed: snapshot.hasData ? bloc.submit : null,
       );
     },
   );
